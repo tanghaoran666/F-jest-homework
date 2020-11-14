da928dbb26d6d5cde3761162bd1143ff7cacef54
tanghaoran666 <18408230362@163.com>
2020-11-14 23:13:47 +0800

    implement test1-12

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,11 @@
+jest.mock("axios")
+import axios from '../../__mocks__/axios';
+import getUsers from '../users';
+
 describe("users", () => {
   test("should get users data with mock axios get", () => {
     // TODO 13: add async test with manual mock
+    getUsers();
+    expect(axios.get).toHaveReturnedWith({name:"thr"})
   });
 });
--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -5,20 +5,21 @@ function forEach(items, callback) {
 test("TODO 11", () => {
   const mockCallback = jest.fn((x) => 42 + x);
   forEach([1, 2], mockCallback);
-
   // TODO 11: add assertion
+  expect(mockCallback).toHaveBeenCalledWith(1);
+  expect(mockCallback).toHaveBeenCalledWith(2);
 });
 
 test("TODO 12", () => {
   const mockFn = jest.fn();
   mockFn
-    .mockReturnValueOnce(42)
-    .mockReturnValueOnce("string")
+  .mockReturnValueOnce(42)
+  .mockReturnValueOnce("string")
     .mockReturnValue(true);
 
   // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe("string");
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
